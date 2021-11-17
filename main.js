836631a72a082d13bcb2abc926bf70caa611b96f
Yevhen <alifanovQA85@gmail.com>
2021-11-17 15:25:00 +0200

    4 lesson

---
+++ main.js
@@ -0,0 +1,84 @@
+const {readFromFile,writeToFile,updateObject} =require('./helper.js');
+let user1 = readFromFile('./myjson.json')[0];
+let user2 = updateObject(user1);
+let user3 = {
+  "firstName": "Bob",
+  "lastName": "Smith",
+  "gender": "male",
+  "age": 35,
+  "children": {
+    "daugthers": [
+      {
+        "name": "Cristal",
+        "gender": "female",
+        "age": 18
+      },
+      {
+        "name": "Hanna",
+        "gender": "female",
+        "age": 5
+      }
+    ],
+    "sons": [
+      {
+        "name": "Mike",
+        "gender": "male",
+        "age": 15
+      }
+    ]
+  },
+  "hobbies": [
+    "cycling",
+    "swimming",
+    "shooting"
+  ],
+  "luckyNumbers": [
+    3,
+    5,
+    15,
+    99,
+    77
+  ]
+};
+
+function deepEqual(elem1, elem2) {
+    if(elem1 === elem2) {
+        return true;
+    }
+    if(typeof elem1 == 'object' && typeof elem2 == 'object' && elem1 != null && elem2 != null) {
+      if(Object.keys(elem1).length == Object.keys(elem2).length) {
+
+        if(Array.isArray(elem1) && Array.isArray(elem2)){
+          for(let key in elem1){
+            if(!deepEqual(elem1[key], elem2[key])){
+              return false
+
+            }
+          } return true;
+        } else {
+          for(key in elem1){
+            if(key in elem2){
+              if(!deepEqual(elem1[key], elem2[key])){
+                return false
+  
+              }
+            };
+          }
+        }return true;
+      } //return true;
+    } return false;
+  }
+
+
+function newWriteTo(){
+  if(deepEqual(user1, user2) == true){
+    console.log('True')
+  } else {
+    writeToFile( './writeFile.json', user2)
+  };
+}
+newWriteTo()
+
+
+
+
