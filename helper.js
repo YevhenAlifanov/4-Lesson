f7c3c972a08124efc59619cdb70fecc60d85419f
Yevhen <alifanovQA85@gmail.com>
2021-11-19 14:29:33 +0200

    delete unusefull text

--- helper.js
+++ helper.js
@@ -1,8 +1,6 @@
 
 const fs = require('fs-extra');
-// fs.readFile('./myjson.js', 'utf-8', ('err, jsonString') => {
-//     console.log(jsonString)
-// })
+
 
 function readFromFile(path){
     return fs.readJSONSync(path);
@@ -14,19 +12,6 @@ function writeToFile( path, obj){
 };
 
 
-
-// function updateObject(mainObj){
-//     let objCopi = {};
-    
-//     let key;
-//     for (key in mainObj){
-//         objCopi[key] = mainObj[key];
-//     }
-//     objCopi.hobbies[3] = 'hike';
-//     objCopi.children.sons[0].age = 11;
-//     return objCopi; // ещё один вариант копирования объекта
-// }
-
 function updateObject(mainObj){
     let objCopi = JSON.parse(JSON.stringify(mainObj));
 
@@ -37,13 +22,7 @@ function updateObject(mainObj){
 };
 
 
-
 const copy = updateObject(user);
-// console.log(copy);
 
-
-
-
-// writeToFile('./writeFile.json', copy);
 
 module.exports = {readFromFile,writeToFile,updateObject}; 
